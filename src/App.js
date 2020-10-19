36fbbb0f48d5969decee2bbbec5ce99340f11af2
usama009012 <you@example.com>
2020-10-19 03:53:14 -0700

    change title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Friend from "./components/Friend";
 
 function App() {
   return (
     <div className="App">
      <h1>hello world</h1>
      <h2>this is the react application</h2>
+     <Friend />
     </div>
   );
 }
---
+++ src/components/Friend.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Friend=()=>{
+    return(
+        <div>
+            <h1>this is another react commit</h1>
+
+        </div>
+    );
+}
+export default Friend;
